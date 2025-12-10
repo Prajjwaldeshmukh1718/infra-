7071001622e6aff2fce6a71f9d61bf4b72c9146f
prajjwal <prajjwal@gmail.com>
2025-12-10 21:59:53 +0530

    This is created for infra

---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "AWS_REGION" {
+    default = "ap-southeast-2"
+}
+variable "AWS_AMI" {
+    default = "ami-0b3c832b6b7289e44"
+}
+variable "AWS_AMI_UBUNTU" {
+    default = "ami-0b8d527345fdace59"
+}
+variable "az" {
+    default = "ap-southeast-1a"
+}
+variable "akey" {
+    default = ""
+}
+variable "skey" {
+    default = ""
+}
+variable "pkey" {
+    default = "sydneynew"
+}
+variable "rpass" {
+    default = "111"
+}
